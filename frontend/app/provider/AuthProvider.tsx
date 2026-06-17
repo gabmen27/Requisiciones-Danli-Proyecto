0c87fbab6d1bd57e5705396ec1eaa1fa4d6e2bdb
NEGA-33 <nestor_elias@unitec.edu>
2026-06-16 20:55:06 -0600

    cambios realizados al backend y al frontend

---
+++ frontend/app/provider/AuthProvider.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+// Re-exportamos AuthProvider desde el contexto
+// Esto lo ponemos en la carpeta provider/ para seguir la estructura del proyecto
+// y lo importamos en layout.tsx
+
+export { AuthProvider } from "../context/AuthContext";
